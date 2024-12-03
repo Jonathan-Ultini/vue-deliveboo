805739931466059c371e92f8139731289d0134f7
Jonathan-Ultini <jonathanultini@gmail.com>
2024-12-03 09:07:34 +0100

    test component

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,29 @@
-<script setup>
-
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div id="app">
+    <AppHeader />
+    <AppMain />
+    <AppFooter />
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<script>
+//import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+//import AppFooter from './components/AppFooter.vue';
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+export default {
+  name: 'App',
+  components: {
+    //AppHeader,
+    AppMain,
+    //AppFooter,
+  },
+};
+</script>
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+<style>
+body {
+  font-family: 'Arial', sans-serif;
+  margin: 0;
 }
 </style>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,15 +1,20 @@
-<script>
-export default {
-  data() {
-    return {
-
-    }
-  }
-}
-</script>
-
 <template>
-
+  <main class="py-5">
+    <div class="container">
+      <h2>Benvenuti su DeliveBoo</h2>
+      <p>Seleziona un ristorante e scopri il loro menù!</p>
+    </div>
+  </main>
 </template>
 
-<style scoped></style>
+<script>
+export default {
+  name: 'AppMain',
+};
+</script>
+
+<style scoped>
+main {
+  text-align: center;
+}
+</style>
