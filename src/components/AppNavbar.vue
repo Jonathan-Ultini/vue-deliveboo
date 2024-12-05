6bb226f775f0652473515492ff7f726a9cebaa70
Andrea Gulli <andreagulli11@gmail.com>
2024-12-05 10:02:00 +0100

    add navbar elements animations & effects

--- src/components/AppNavbar.vue
+++ src/components/AppNavbar.vue
@@ -25,7 +25,7 @@ export default {
       <li><a href="#"><i class="fas fa-envelope"></i> Contatti</a></li>
       <li><a href="#"><i class="fas fa-user-pen"></i> Registrati</a></li>
       <li><a href="#"><i class="fas fa-arrow-right-to-bracket"></i> Accedi</a></li>
-      <li><a href="#"><i class="fas fa-cart-shopping"></i> Carrello</a></li>
+      <li><a href="#"><i class="fas fa-cart-shopping"></i></a></li>
 
     </ul> 
   </div>
@@ -41,35 +41,52 @@ $custom-yellow: rgb(255, 214, 58);
   max-width: 90%;
 }
 
-// Unordered List
-
+// Unordered List, ul elements
 ul li {
   list-style: none;
 }
 
 a{
-  padding: 10px 15px;
+  padding: 8px 15px;
+
+  color: black;
   text-decoration: none;
   background-color: $custom-yellow;
   border-radius: 15px;
 
   display: inline-block;
-  transition: transform 0.5s ease;
+  transition: transform 0.3s ease, background-color 0.3s ease, box-shadow 0.3s ease; // Aggiungi altre proprietà
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
 }
 
 a:hover{
   transform: scale(1.05);
+  background-color: rgb(255, 240, 170);
+  box-shadow: 0 8px 12px rgba(0, 0, 0, 0.2);
+}
+
+a:active{
+  transform: scale(0.95);
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  background-color: $custom-yellow;
+  transition: transform 0.2s ease, box-shadow 0.2s ease, background-color 0.2s ease;
+}
+
+a:hover i {
+  transform: rotate(10deg); // Rotazione leggera
+  transition: transform 0.3s ease; 
 }
 
 i{
-  margin-right: 5px;
+  margin-right: 4px;
 }
 
 // Figure, img
 img{
   margin: 0;
   padding: 0;
-  max-width: 100%
+  max-width: 100%;
+  height: auto;
 }
 
 figure{
