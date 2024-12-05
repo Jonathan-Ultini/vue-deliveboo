2716be7f81794e55b7e5c8d3bdd8de0bba89772d
Jonathan-Ultini <jonathanultini@gmail.com>
2024-12-05 11:21:39 +0100

    add route to homepage

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,17 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import Login from '../components/Login.vue';
-import Register from '../components/Register.vue';
-import Dashboard from '../components/Dashboard.vue';
-import Dishes from '../components/Dishes.vue';
+import { createRouter, createWebHistory } from "vue-router";
+import HomePage from "../components/HomePage.vue";
 
 const routes = [
-  { path: '/', redirect: '/login' },
-  { path: '/login', component: Login },
-  { path: '/register', component: Register },
-  { path: '/dashboard', component: Dashboard, meta: { requiresAuth: true } },
-  { path: '/dishes', component: Dishes, meta: { requiresAuth: true } },
+  {
+    path: "/",
+    name: "Home",
+    component: HomePage,
+  },
 ];
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
 });
-
 
 export default router;
