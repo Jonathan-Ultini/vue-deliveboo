1d749e75a36333d1b87157cb448155b1c4b7e8bb
JoseManuel-Feliz <jmgarcia2120@gmail.com>
2024-12-12 09:42:07 +0100

    feat: added state for cart initialization and getters for total and item count

--- src/store/cartStore.js
+++ src/store/cartStore.js
@@ -2,11 +2,29 @@ import { defineStore } from 'pinia';
 
 export const useCartStore = defineStore('cart', {
     state: () => ({
-
+        cart: JSON.parse(localStorage.getItem('cart')) || {
+            restaurantId: null,
+            items: []
+        },
     }),
-    getters: {
 
+    getters: {
+        total: (state) => {
+            let totalSum = 0;
+            for (const item of state.cart.items) {
+                totalSum += item.price * item.quantity;
+            }
+            console.log('Computed Total:', totalSum.toFixed(2)); // Log del totale
+            return totalSum.toFixed(2);
+        },
+        itemCount: (state) => {
+            console.log('Item Count:', state.cart.items.length); // Log del conteggio
+            return state.cart.items.length;
+        },
     }, actions: {
 
     },
 });
+
+
+
