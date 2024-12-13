1d60dd2641505cefac6cd5340d5accc71b16efd5
JoseManuel-Feliz <jmgarcia2120@gmail.com>
2024-12-13 02:50:47 +0100

    refactor: improved cart management with new methods, lifecycle hooks, and reactivity

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,19 @@
   <div>
     <AppHeader :cartCount="cartCount" @toggle-cart="toggleCart" />
 
-    <CartSidebar v-if="showCart" :cart="cartStore.cart" @update-cart="updateCart" @close="toggleCart" />
+    <CartSidebar v-if="showCart" :cart="cartItems" @update-quantity="handleUpdateQuantity"
+      @remove-item="handleRemoveItem" @close="toggleCart" />
 
     <AppFooter />
   </div>
 </template>
 
 <script>
-import AppHeader from './components/AppHeader.vue';
-import AppFooter from './components/AppFooter.vue';
-import CartSidebar from './components/CartSidebar.vue';
-import { useCartStore } from '@/store/cartStore';
-import { computed } from 'vue';
+import AppHeader from "./components/AppHeader.vue";
+import AppFooter from "./components/AppFooter.vue";
+import CartSidebar from "./components/CartSidebar.vue";
+import { useCartStore } from "@/store/cartStore";
+import { computed, onMounted, onBeforeUnmount } from "vue";
 
 export default {
   components: {
@@ -25,24 +26,37 @@ export default {
     const cartStore = useCartStore();
 
     const cartCount = computed(() => cartStore.itemCount);
-
+    const cartItems = computed(() => cartStore.sortedItems); // Usa il getter ordinato
     const showCart = computed(() => cartStore.showCart);
 
     const toggleCart = () => {
-      cartStore.showCart = !cartStore.showCart;
+      cartStore.toggleCartSidebar();
     };
 
-    const updateCart = (updatedCart) => {
-      cartStore.cart = updatedCart;
-      cartStore.saveCart();
+    const handleUpdateQuantity = (dishId, changeAmount) => {
+      cartStore.updateQuantity(dishId, changeAmount);
     };
+
+    const handleRemoveItem = (dishId) => {
+      cartStore.removeFromCart(dishId);
+    };
+
+    onMounted(() => {
+      cartStore.loadCart();
+      cartStore.setupBeforeUnload();
+    });
+
+    onBeforeUnmount(() => {
+      cartStore.teardownBeforeUnload();
+    });
 
     return {
-      cartStore,
       cartCount,
+      cartItems,
       toggleCart,
       showCart,
-      updateCart,
+      handleUpdateQuantity,
+      handleRemoveItem,
     };
   },
 };
