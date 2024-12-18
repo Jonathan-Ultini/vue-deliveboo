5695abac28d1e3265f321790ccc38b6480e17f2a
Jonathan-Ultini <jonathanultini@gmail.com>
2024-12-18 08:16:33 +0100

    some fix to checkout page

--- src/components/Checkout.vue
+++ src/components/Checkout.vue
@@ -9,7 +9,7 @@
         <p><strong>Address:</strong> {{ cart.restaurantAddress }}</p>
         <div class="cart-items">
           <p><strong>Dishes:</strong></p>
-          <div v-for="item in cart.items" :key="item.id" class="cart-item">
+          <div v-for="item in cartStore.items" :key="item.id" class="cart-item">
             <p><strong>{{ item.name }}</strong> - {{ item.quantity }} x {{ item.price }} €</p>
           </div>
         </div>
@@ -17,9 +17,15 @@
           <h4>Total: {{ totalCount }} €</h4>
         </div>
       </div>
-      <button class="btn btn-primary " @click="nextStep">Continue</button>
-      <button class="btn btn-secondary m-2" @click="cancel">Cancel
-      </button>
+      <div v-if="cartStore.items && cartStore.items.length === 0">
+        <p class="text-center">Your cart is empty. Explore the menu to start ordering!</p>
+        <button class="btn btn-primary" @click="cancel">Go to Homepage</button>
+      </div>
+      <div v-else>
+        <button class="btn btn-primary" @click="nextStep">Continue</button>
+        <button class="btn btn-secondary m-2" @click="cancel">Cancel</button>
+      </div>
+
     </div>
 
     <!-- Step 2: Informazioni Ordine -->
@@ -54,7 +60,7 @@
         </div>
         <div class="col-md-4">
           <h4 class="my-4">Cart summary</h4>
-          <div v-for="item in cart.items" :key="item.id">
+          <div v-for="item in cartStore.items" :key="item.id">
             <p><strong>{{ item.name }}</strong> - {{ item.quantity }} x {{ item.price }} €</p>
           </div>
           <h5>Total: {{ totalCount }} €</h5>
@@ -99,8 +105,8 @@ import { useCartStore } from "@/stores/cartStore";
 
 export default {
   setup() {
-    const cart = useCartStore();
-    return { cart };
+    const cartStore = useCartStore();
+    return { cartStore };
   },
   data() {
     return {
@@ -381,7 +387,7 @@ export default {
         }
       } catch (error) {
         console.error("Error during payment:", error);
-        alert("An error occurred during payment.");
+        //alert("An error occurred during payment.");
       } finally {
         this.loading = false; // Stato di caricamento disattivato
       }
